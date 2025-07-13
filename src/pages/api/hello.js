47203e06bbc1db52a7d28f316b6b98f3947dd9c5
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-13 21:49:17 +0200

    connection through api success'

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import dbConnect from "../../../db/connect";
 import Test from "../../../db/Schema/Test";
-
+ 
 export default async function handler(req, res) {
-  await dbConnect()
-  const tests = await Test.find()
+  await dbConnect();
+  const tests = await Test.find();
   res.status(200).json(tests);
 }
